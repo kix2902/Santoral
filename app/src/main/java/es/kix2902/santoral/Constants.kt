3325e2e06ea9d835baf1be9e01f369a746c5edf2
Ismael Reyes <ismael.kix2902@gmail.com>
2019-06-02 21:47:32 +0200

    Added some constants and strings needed in repository classes

--- app/src/main/java/es/kix2902/santoral/Constants.kt
+++ app/src/main/java/es/kix2902/santoral/Constants.kt
@@ -1,3 +1,5 @@
 package es.kix2902.santoral
 
-const val PRIVACY_URL = "https://base.kix2902.es/santoral/privacy_policy.html"
+const val SANTOPEDIA_API_URL = "http://api.santopedia.com"
+const val SANTOPEDIA_API_APP_ID = "8dc38a18"
+const val SANTOPEDIA_API_APP_KEY = "032bd39671730c224be2354c8981fb4c"
