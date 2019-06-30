5909a3b22d9b0b924611b63bfdc2746c6607317a
Ismael Reyes <ismael.kix2902@gmail.com>
2019-06-30 20:45:38 +0200

    Updated Santoral Application implementation with repository to load data

--- app/src/main/java/es/kix2902/santoral/SantoralApplication.kt
+++ app/src/main/java/es/kix2902/santoral/SantoralApplication.kt
@@ -2,28 +2,28 @@ package es.kix2902.santoral
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.preference.PreferenceManager
+import es.kix2902.santoral.data.DataRepository
 
 class SantoralApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
-        val themeModePreference = prefs.getString("theme-mode", "")!!
-
-        when (themeModePreference) {
-            "never" -> {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            }
-            "battery" -> {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
-            }
-            "system" -> {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-            }
-            "always" -> {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        val repository = DataRepository.getInstance(this)
+        repository.getThemeMode { value ->
+            when (value) {
+                "never" -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+                "battery" -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+                }
+                "system" -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                }
+                "always" -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                }
             }
         }
     }
