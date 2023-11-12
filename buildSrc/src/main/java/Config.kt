0aa8dea288d427c9958c4ec8cec9545442f430a0
Ismael Reyes <ismael.kix2902@gmail.com>
2023-11-12 15:17:13 +0100

    Update to Android 14

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -2,9 +2,9 @@ object Config {
     const val appId="es.kix2902.santoral"
 
     const val minSdk = 23
-    const val targetSdk = 32
-    const val compileSdk = 32
+    const val targetSdk = 34
+    const val compileSdk = 34
 
-    const val versionCode = 60
-    const val versionName = "5.0.1"
+    const val versionCode = 61
+    const val versionName = "5.0.2"
 }
